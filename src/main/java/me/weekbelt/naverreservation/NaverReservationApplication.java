45992cab9c81e75b0452e06620274b8f8a065d03
joohyuk <vfrvfr4207@gmail.com>
2019-12-27 12:10:51 +0900

    Initial commit

---
+++ src/main/java/me/weekbelt/naverreservation/NaverReservationApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.naverreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/weekbelt/naverreservation/NaverReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.naverreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
